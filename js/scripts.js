88cb14fe2a0fbfa8a27c5b3ad2407f489fa13353
tiegrun <tiko3020@gmail.com>
2020-08-13 02:10:04 -0400

    achievement 1-1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+alert('Hello world');
+
+var favoriteFood = "pizza";
+
+document.write(favoriteFood);
+
+document.body.style.height = "100vw";
+document.body.style.height = "100vh";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.fontSize = "6rem"
+document.body.style.textTransform = "uppercase";
+document.body.style.background = "#ccc";
+document.body.style.color = "blue";
