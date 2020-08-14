2c0b17583148c4e83017eb0fb141a4c1adc71c76
tiegrun <tiko3020@gmail.com>
2020-08-14 02:42:51 -0400

    created Pokemon list
    created 6 Pokémon objects in pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,50 @@
-alert('Hello world');
+var pokemonList = [];
+pokemonList[0] = {
+  name: "Venusaur",
+  height: 2,
+  weight: 100,
+  types: ["grass", "poison"],
+  abilities: ["chlorophyll", "overgrow"]
+};
 
-var favoriteFood = "pizza";
+pokemonList[1] = {
+  name: "Charizard",
+  height: 1.7,
+  weight: 90.5,
+  types: ["fire", "flying"],
+  abilities: ["blaze", "solar power"],
+};
 
-document.write(favoriteFood);
+pokemonList[2] = {
+  name: "Pidgeot",
+  height: 1.5,
+  weight: 39.5,
+  types: ["flying", "normal"],
+  abilities: ["keen eye", "tangled feet", "big pecks"]
+};
 
-document.body.style.height = "100vw";
-document.body.style.height = "100vh";
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-document.body.style.alignItems = "center";
-document.body.style.fontSize = "6rem"
-document.body.style.textTransform = "uppercase";
-document.body.style.background = "#ccc";
-document.body.style.color = "blue";
+pokemonList[3] = {
+  name: "Fearow",
+  height: 1.2,
+  weight: 38,
+  types: ["flying", "normal"],
+  abilities: ["keen eye", "sniper"]
+};
+
+pokemonList[4] = {
+  name: "Nidoking",
+  height: 1.4,
+  weight: 62,
+  types: ["ground", "poison"],
+  abilities: ["poison point", "rivalry", "sheer force"]
+};
+
+pokemonList[5] = {
+  name: "Slowbro",
+  height: 1.6,
+  weight: 78.5,
+  types: ["psychic", "water"],
+  abilities: ["oblivious", "own tempo", "regenerator"]
+};
+
+console.log(pokemonList.length);
