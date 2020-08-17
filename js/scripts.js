89a8c739a1fa84221534263eb9737e0078110a31
tiegrun <tiko3020@gmail.com>
2020-08-17 03:05:24 -0400

    new style

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,55 @@
-var pokemonList = [];
-pokemonList[0] = {
-  name: "Venusaur",
-  height: 2,
-  weight: 100,
-  types: ["grass", "poison"],
-  abilities: ["chlorophyll", "overgrow"]
-};
+var pokemonList = [ 
+                    {
+                      name: "Venusaur",
+                      height: 2,
+                      weight: 100,
+                      types: ["grass", "poison"],
+                      abilities: ["chlorophyll", "overgrow"]
+                    },
+  
+                    {
+                      name: "Charizard",
+                      height: 1.7,
+                      weight: 90.5,
+                      types: ["fire", "flying"],
+                      abilities: ["blaze", "solar power"],
+                    },
+                    {
+                      name: "Pidgeot",
+                      height: 1.5,
+                      weight: 39.5,
+                      types: ["flying", "normal"],
+                      abilities: ["keen eye", "tangled feet", "big pecks"]
+                    },
+                    {
+                      name: "Fearow",
+                      height: 1.2,
+                      weight: 38,
+                      types: ["flying", "normal"],
+                      abilities: ["keen eye", "sniper"]
+                    },
+                    {
+                      name: "Nidoking",
+                      height: 1.4,
+                      weight: 62,
+                      types: ["ground", "poison"],
+                      abilities: ["poison point", "rivalry", "sheer force"]
+                    },
+                    {
+                      name: "Slowbro",
+                      height: 1.6,
+                      weight: 78.5,
+                      types: ["psychic", "water"],
+                      abilities: ["oblivious", "own tempo", "regenerator"]
+                    }
+                  ];
 
-pokemonList[1] = {
-  name: "Charizard",
-  height: 1.7,
-  weight: 90.5,
-  types: ["fire", "flying"],
-  abilities: ["blaze", "solar power"],
-};
-
-pokemonList[2] = {
-  name: "Pidgeot",
-  height: 1.5,
-  weight: 39.5,
-  types: ["flying", "normal"],
-  abilities: ["keen eye", "tangled feet", "big pecks"]
-};
-
-pokemonList[3] = {
-  name: "Fearow",
-  height: 1.2,
-  weight: 38,
-  types: ["flying", "normal"],
-  abilities: ["keen eye", "sniper"]
-};
-
-pokemonList[4] = {
-  name: "Nidoking",
-  height: 1.4,
-  weight: 62,
-  types: ["ground", "poison"],
-  abilities: ["poison point", "rivalry", "sheer force"]
-};
-
-pokemonList[5] = {
-  name: "Slowbro",
-  height: 1.6,
-  weight: 78.5,
-  types: ["psychic", "water"],
-  abilities: ["oblivious", "own tempo", "regenerator"]
-};
-
-console.log(pokemonList.length);
-
-for(i=0 ; i<pokemonList.length ; i++){
-
-  if(pokemonList[i].height > 1.9){
-    pokemonList[i].height += " ( Wow, that’s big! ) ";
-    document.write(pokemonList[i].name + " " + " - " + pokemonList[i].height + " ; " + "<br><br>" );
+pokemonList.forEach(function(pokemon){
+  if(pokemon.height > 1.9){
+    pokemon.height += " ( Wow, that’s big! ) ";
+    document.write(pokemon.name + " " + " - " + pokemon.height + " ; " + "<br><br>" );
   }
     else{
-      document.write(pokemonList[i].name + " " + " - " + pokemonList[i].height + " ; " + "<br><br>"  );
+      document.write(pokemon.name + " " + " - " + pokemon.height + " ; " + "<br><br>" );
     }
-}
+})
