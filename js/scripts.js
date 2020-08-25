223bb065a3c4393bca5185cc36b3bcc8d2f75193
Tigran <61937515+tiegrun@users.noreply.github.com>
2020-08-25 02:41:49 -0400

    look master branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-var pokemonList = [ 
-                    {
-                      name: "Venusaur",
-                      height: 2,
-                      weight: 100,
-                      types: ["grass", "poison"],
-                      abilities: ["chlorophyll", "overgrow"]
-                    },
-  
-                    {
-                      name: "Charizard",
-                      height: 1.7,
-                      weight: 90.5,
-                      types: ["fire", "flying"],
-                      abilities: ["blaze", "solar power"],
-                    },
-                    {
-                      name: "Pidgeot",
-                      height: 1.5,
-                      weight: 39.5,
-                      types: ["flying", "normal"],
-                      abilities: ["keen eye", "tangled feet", "big pecks"]
-                    },
-                    {
-                      name: "Fearow",
-                      height: 1.2,
-                      weight: 38,
-                      types: ["flying", "normal"],
-                      abilities: ["keen eye", "sniper"]
-                    },
-                    {
-                      name: "Nidoking",
-                      height: 1.4,
-                      weight: 62,
-                      types: ["ground", "poison"],
-                      abilities: ["poison point", "rivalry", "sheer force"]
-                    },
-                    {
-                      name: "Slowbro",
-                      height: 1.6,
-                      weight: 78.5,
-                      types: ["psychic", "water"],
-                      abilities: ["oblivious", "own tempo", "regenerator"]
-                    }
-                  ];
-
-pokemonList.forEach(function(pokemon){
-  if(pokemon.height > 1.9){
-    pokemon.height += " ( Wow, that’s big! ) ";
-    document.write(pokemon.name + " " + " - " + pokemon.height + " ; " + "<br><br>" );
-  }
-    else{
-      document.write(pokemon.name + " " + " - " + pokemon.height + " ; " + "<br><br>" );
-    }
-})
-=======
 var pokemonRepository = (function(){
   var pokemonList = [ 
     {
@@ -151,5 +94,3 @@ pokemonRepository.add(parasect);
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.addListItem(pokemon.name);
 })
-
->>>>>>> master
